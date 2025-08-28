beb6f780ba31c6b65de002d68f546094344b22cd
areeb-saqib <areeb.saqib@ccript.com>
2025-08-28 21:44:24 +0500

    fix: update size limits

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,8 @@ async function main() {
   app.use(express.json());
   app.use(cors(corsOptions));
   app.use(passport.initialize());
-  app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ limit: "50mb", extended: true }));
+  app.use(express.json({ limit: "15mb" }));
+  app.use(express.urlencoded({ limit: "15mb", extended: true }));
 
   app.use("/api/v1/launch-mails/", launchMailsRouter);
   app.use("/api/v1/newsletter-mails/", newsletterMailsRouter);
